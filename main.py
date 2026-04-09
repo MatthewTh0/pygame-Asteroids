8572ed50549bcba8a87533f0f9cef2831b08ace4
MatthewTh0 <matthornton@live.com>
2026-04-09 16:13:09 -0500

    Added player and circleshape classes, added constants,  implemented them in main,

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from logger import log_state
+from player import Player
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 
 def main():
@@ -7,7 +8,11 @@ def main():
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
     pygame.init()
+    timer = pygame.time.Clock()
+    # delta time since last frame was drawn
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    yourPlayer = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2)
 
     while True:
         log_state()
@@ -15,7 +20,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        yourPlayer.draw(screen)
         pygame.display.flip()
+        dt = timer.tick(60)/1000
+        # print(dt)
 
 if __name__ == "__main__":
     main()
